22eac8a760a3c47b4e15d1b25b0493924e117168
hhh3167253066 <3167253066@qq.com>
2022-11-17 23:06:19 +0800

    两套路由的配置

--- vue2-demos/src/router/index.js
+++ vue2-demos/src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Index from '../views/Index.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+// 配置两套路由online与h5
+const routes_online = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Index',
+    component: Index
   },
   {
     path: '/iframe',
@@ -16,6 +17,24 @@ const routes = [
     component: () => import('../views/IframeView.vue')
   }
 ]
+
+const routes_h5 = [
+  {
+    path: '/',
+    name: 'Index',
+    component: Index
+  },
+  {
+    path: '/iframe',
+    name: 'iframe',
+    component: () => import('../views/IframeView.vue')
+  }
+]
+
+let routes = routes_online;
+if (navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
+  routes = routes_h5
+}
 
 const router = new VueRouter({
   routes
