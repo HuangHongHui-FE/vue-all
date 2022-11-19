f2cd430bf8b908dfd3229ed57684b44bad578e4a
hhh3167253066 <3167253066@qq.com>
2022-11-19 22:47:48 +0800

    外部url config

---
+++ vue2-demos/src/config/url.js
@@ -0,0 +1,6 @@
+export const vue_all_url = 'https://gitee.com/hhh3167253066/vue-all'
+export const react_all_url = 'https://gitee.com/hhh3167253066/react-all'
+export const node_all_url = 'https://gitee.com/hhh3167253066/node-all'
+export const vue3_visual_url = 'https://gitee.com/hhh3167253066/xiaoman-vue3-visual'
+export const gitee_url = 'https://gitee.com/hhh3167253066/'
+export const vue3_constelation_url = 'https://gitee.com/hhh3167253066/constelation'
